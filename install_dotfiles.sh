cf05d19348dd596871a0ff29c02580b19bc9cd46
Nick Haglind <nickhaglind@nicks-air.lan>
2024-11-11 17:09:23 -0600

    Add dot files with script to copy over

---
+++ install_dotfiles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Copying Alacritty config..."
+cp -r ~/dotfiles/alacritty ~/.config/alacritty
+
+echo "Copying Neovim config..."
+cp -r ~/dotfiles/nvim ~/.config/nvim
+
+echo "Copying Zellij config..."
+cp -r ~/dotfiles/zellij ~/.config/zellij
+
+echo "Dotfiles have been copied to ~/.config!"
