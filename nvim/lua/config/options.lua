d33b0ccb865eac6e4e29d11d666cdfcc6fdcf342
Nick Haglind <nhaglind@gmail.com>
2024-11-15 08:59:52 -0600

    Update dotfiles - 2024-11-15 08:59:52

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.fixeol = true -- Automatically add newline at end of file
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.trimtrailing = true -- Trim trailing whitespace
+
+-- If you want to configure formatting options more broadly:
+vim.g.autoformat = true -- Keep LazyVim's automatic formatting enabled
