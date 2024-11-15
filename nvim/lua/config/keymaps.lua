2e9f859a96d6d514b488923349167e7627c0a86a
Nick Haglind <nhaglind@gmail.com>
2024-11-15 08:05:12 -0600

    Update dotfiles - 2024-11-15 08:05:12

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Quick save with 'ss' in normal mode
+keymap.set("n", "ss", ":w<CR>", { desc = "Save file" })
+
+-- Save and close buffer with 'sq' (save-quit)
+keymap.set("n", "sq", ":w<CR>:bd<CR>", { desc = "Save and close buffer" })
